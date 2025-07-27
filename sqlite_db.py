ec0c2df5b5102d0f34c432ecefb388d18fe95dcb
servomouse <servomouse@protonmail.com>
2025-07-27 12:23:27 -0600

    Create sqlite DB interface

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,24 +1,26 @@
 import sqlite3
 
-
-component_table_struct = {
-    "id":               "INTEGER PRIMARY KEY AUTOINCREMENT",
-    "category":         "TEXT NOT NULL",
-    "name":             "TEXT",
-    "case":             "TEXT",
-    "value":            "TEXT",
-    "precision":        "TEXT",
-    "voltage":          "TEXT",
-    "technology":       "TEXT",
-    "description":      "TEXT",
-    "image":            "TEXT",
-    "kicad_footprint":  "TEXT",
-    "kicad_symbol":     "TEXT",
-    "model_3d":         "TEXT",
-    "amount":           "INTEGER",
+tables_structs = {
+    "components": {
+        "id":               "INTEGER PRIMARY KEY AUTOINCREMENT",
+        "category":         "TEXT NOT NULL",
+        "subcategory":      "TEXT NOT NULL",
+        "group_name":       "TEXT NOT NULL",
+        "name":             "TEXT",
+        "package":          "TEXT",
+        "value":            "TEXT",
+        "precision":        "TEXT",
+        "voltage":          "TEXT",
+        "technology":       "TEXT",
+        "description":      "TEXT",
+        "image":            "TEXT",
+        "kicad_footprint":  "TEXT",
+        "kicad_symbol":     "TEXT",
+        "model_3d":         "TEXT",
+        "amount":           "INTEGER",
+    },
+    "updates": {}
 }
-
-updates_table_struct = {}
 
 
 class SQLiteIface:
@@ -27,86 +29,152 @@ class SQLiteIface:
     
     def execute_command(self, cmd, args: tuple):
         try:
-            connection = sqlite3.connect('components.db')
-            cursor = connection.cursor()
-            cursor.execute(cmd, args)
-            connection.commit()
-            connection.close()
+            with sqlite3.connect(self.db) as conn:
+                cursor = conn.cursor()
+                if args is None:
+                    cursor.execute(cmd)
+                else:
+                    cursor.execute(cmd, args)
+                conn.commit()
         except sqlite3.Error as e:
             print(f"An error occurred: {e}")
-        finally:
-            connection.close()
 
     def table_exists(self, table_name):
-        connection = sqlite3.connect('components.db')
-        cursor = connection.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
-        result = cursor.fetchone()
-        connection.close()
-        return result is not None
+        try:
+            with sqlite3.connect(self.db) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+                result = cursor.fetchone()
+            return result is not None
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+    
+    def create_table(self, table_name):
+        '''
+            Creates table table_name if not exists, otherwise does nothing
+        '''
+        if table_name not in tables_structs:
+            print(f"Error: couldn't find table structure for {table_name}!")
+            return
+        command = f'CREATE TABLE IF NOT EXISTS {table_name} ('
+        for key, value in tables_structs[table_name].items():
+            command += f" {key} {value},"
+        command = command[:-1] + ')'
+        self.execute_command(command, None)
 
     def add_component(self, data):
-        self.execute_command(
-            'INSERT INTO components (name, type, value, description) VALUES (?, ?, ?, ?)',
-            (name, type, value, description)
-        )
+        keys = list(tables_structs['components'].keys())
+        keys.remove('id')
+        values = []
+        for key in keys:
+            values.append(data[key])
+        command = 'INSERT INTO components (' + ', '.join(keys) + ') VALUES (' + ', '.join(['?' for _ in range(len(keys))]) + ')'
+        self.execute_command(command, tuple(values))
 
     def remove_component(self, component_id):
         self.execute_command('DELETE FROM components WHERE id = ?', (component_id,))
 
-    def edit_component(self, component_id, name, type, value, description):
-        self.execute_command('''
-            UPDATE components
-            SET name = ?, type = ?, value = ?, description = ?
-            WHERE id = ?
-            ''',
-            (name, type, value, description, component_id)
-        )
-
+    def edit_component(self, data):
+        keys = list(tables_structs['components'].keys())
+        keys.remove('id')
+        values = []
+        for key in keys:
+            values.append(data[key])
+        values.append(data['id'])
+        self.execute_command(f"UPDATE components SET {' = ?, '.join(keys)} = ? WHERE id = ?", tuple(values))
 
     def export_table_to_dict(self, table_name):
-        connection = sqlite3.connect('components.db')
-        cursor = connection.cursor()
-        cursor.execute(f'SELECT * FROM {table_name}')
-        rows = cursor.fetchall()
-        column_names = [description[0] for description in cursor.description]
-        result = []
-        for row in rows:
-            row_dict = {column_names[i]: row[i] for i in range(len(column_names))}
-            result.append(row_dict)
-        connection.close()
-        return result
+        try:
+            with sqlite3.connect(self.db) as conn:
+                cursor = conn.cursor()
+                cursor.execute(f'SELECT * FROM {table_name}')
+                rows = cursor.fetchall()
+                column_names = [description[0] for description in cursor.description]
+                result = []
+                for row in rows:
+                    row_dict = {column_names[i]: row[i] for i in range(len(column_names))}
+                    result.append(row_dict)
+            return result
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
 
 
     def get_last_n_entries(self, table_name, n=10):
-        connection = sqlite3.connect('components.db')
-        cursor = connection.cursor()
+        try:
+            with sqlite3.connect(self.db) as conn:
+                cursor = conn.cursor()
+                cursor.execute(f'SELECT * FROM {table_name} ORDER BY id DESC LIMIT ?', (n,))
+                rows = cursor.fetchall()
+                column_names = [description[0] for description in cursor.description]
 
-        cursor.execute(f'SELECT * FROM {table_name} ORDER BY id DESC LIMIT ?', (n,))
-
-        rows = cursor.fetchall()
-        column_names = [description[0] for description in cursor.description]
-
-        result = []
-        for row in rows:
-            row_dict = {column_names[i]: row[i] for i in range(len(column_names))}
-            result.append(row_dict)
-        connection.close()
-        return result
+                result = []
+                for row in rows:
+                    row_dict = {column_names[i]: row[i] for i in range(len(column_names))}
+                    result.append(row_dict)
+            return result
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
 
 
-if __name__ == "__main__":
-    connection = sqlite3.connect('components.db')
-    cursor = connection.cursor()
+test_comp_list = [
+    # {
+    #     "category":         "ICs",
+    #     "subcategory":      "times",
+    #     "group_name":       "",
+    #     "name":             "NE555",
+    #     "package":          "DIP-8",
+    #     "value":            "",
+    #     "precision":        "",
+    #     "voltage":          "",
+    #     "technology":       "CMOS",
+    #     "description":      "555 Timer IC",
+    #     "image":            "",
+    #     "kicad_footprint":  "",
+    #     "kicad_symbol":     "",
+    #     "model_3d":         "",
+    #     "amount":           12,
+    # },
+    {
+        "category":         "Resistors",
+        "subcategory":      "",
+        "group_name":       "",
+        "name":             "",
+        "package":          "0805",
+        "value":            "10k",
+        "precision":        "1%",
+        "voltage":          "",
+        "technology":       "",
+        "description":      "100k SMD resistor",
+        "image":            "",
+        "kicad_footprint":  "",
+        "kicad_symbol":     "",
+        "model_3d":         "",
+        "amount":           10,
+    },
+    {
+        "category":         "Capacitors",
+        "subcategory":      "",
+        "group_name":       "",
+        "name":             "",
+        "package":          "0603",
+        "value":            "10n",
+        "precision":        "",
+        "voltage":          "15V",
+        "technology":       "Ceramic",
+        "description":      "100n ceramic SMD capacitor",
+        "image":            "",
+        "kicad_footprint":  "",
+        "kicad_symbol":     "",
+        "model_3d":         "",
+        "amount":           24,
+    }
+]
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS components (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        type TEXT NOT NULL,
-        value TEXT NOT NULL,
-        description TEXT
-    )
-    ''')
-    connection.commit()
-    connection.close()
+
+# if __name__ == "__main__":
+#     db = SQLiteIface('database/components.db')
+#     db.create_table('components')
+#     print(f"{db.export_table_to_dict('components') = }")
+#     for comp in test_comp_list:
+#         db.add_component(comp)
+#     print(f"{db.export_table_to_dict('components') = }")
