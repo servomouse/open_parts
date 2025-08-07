927f2aea89714282eb9cd3fe0950fac0677fec83
servomouse <servomouse@protonmail.com>
2025-08-06 18:14:02 -0600

    Refactor the showComponentInfo function

--- static/scripts/component-info-panel.js
+++ static/scripts/component-info-panel.js
@@ -1,70 +1,10 @@
+import { fetchComponents, upate_component } from './network.js';
+import { updateComponents, getComponentsStats } from './components.js';
+import { sortComponents } from './sorting.js';
+import { displayComponents } from './display_components.js';
 
-// <section id="component-details">
-    // <div class="component-info">
-    //     <h2>Component Details</h2>
-    //     <h3 id="component-name">10k Ohm Resistor</h3>
-    //     <ul>
-    //         <li>
-    //             <strong class="component-info-field-name">Category:</strong>
-    //             <span class="component-info-input-span">Resistor</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Value:</strong>
-    //             <span class="component-info-input-span">10k</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Precision:</strong>
-    //             <span class="component-info-input-span">1%</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Package:</strong>
-    //             <span class="component-info-input-span">0603</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Technology:</strong>
-    //             <span class="component-info-input-span">Film</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Amount Available:</strong>
-    //             <span class="component-info-input-span">10</span>
-    //         </li>
-    //         <li>
-    //             <strong class="component-info-field-name">Description:</strong>
-    //             <span class="component-info-input-span">Standard 10k Ohm resistor.</span>
-    //         </li>
-    //         <li class="has-copy-symbol">
-    //             <strong class="component-info-field-name">KiCAD footprint:</strong>
-    //             <span class="component-info-input-span">path/to/footprint</span>
-    //             <span class="copy-symbol" onclick="copyToClipboard(this)" title="Copy to clipboard">&#10064;</span>
-    //         </li>
-    //         <li class="has-copy-symbol">
-    //             <strong class="component-info-field-name">KiCAD symbol:</strong> 
-    //             <span class="component-info-input-span">path/to/symbol</span>
-    //             <span class="copy-symbol" onclick="copyToClipboard(this)" title="Copy to clipboard">&#10064;</span>
-    //         </li>
-    //         <li class="has-copy-symbol">
-    //             <strong class="component-info-field-name">Image:</strong>
-    //             <span class="component-info-input-span">path/to/image</span>
-    //             <span class="copy-symbol" onclick="copyToClipboard(this)" title="Copy to clipboard">&#10064;</span>
-    //         </li>
-    //         <li class="has-copy-symbol">
-    //             <strong class="component-info-field-name">3D model:</strong>
-    //             <span class="component-info-input-span">path/to/3d_model</span>
-    //             <span class="copy-symbol" onclick="copyToClipboard(this)" title="Copy to clipboard">&#10064;</span>
-    //         </li>
-    //     </ul>
-    //     <button id="component-info-edit-button" onclick="toggleEdit()">Edit</button>
-    // </div>
-    // <div class="image-and-model-container">
-    //     <div class="component-image">
-    //         <img src="{{ url_for('static', filename='images/resistor_1w.jpg') }}" alt="10k Ohm Resistor" id="component-image">
-    //     </div>
-    //     <div class="component-3d-model" id="component-3d-model" style="border: 1px solid #ccc; padding: 10px; height: 200px;">
-    //         <model-viewer src="{{ url_for('static', filename='3d_models/octal_socket.glb') }}" alt="3D model of 10k Ohm Resistor" auto-rotate camera-controls style="width: 100%; height: 100%; background-color: #909090;" exposure="0.5" >
-    //         </model-viewer>
-    //     </div>
-    // </div>
-// </section>
+let currentComponentId = 0;
+let currentComponent = null;
 
 function copyToClipboard(button) {
     // Find the span element next to the button
@@ -82,7 +22,7 @@ function copyToClipboard(button) {
     alert('Copied: ' + span.innerText);
 }
 
-function toggleFields(editable) {
+async function toggleFields(editable) {
     const fields = document.querySelectorAll('.component-info-input');
     fields.forEach(field => {
         field.readOnly = !editable; // Toggle readOnly attribute
@@ -94,7 +34,9 @@ function toggleFields(editable) {
             const input = document.createElement('input');
             input.type = 'text';
             input.value = span.innerText; // Set the input value to the current text
-            input.className = 'component-info-input'; // Optional: add a class for styling
+            input.className = 'component-info-input';
+            input.id = span.id;
+            // console.log(`Span -> input id: ${input.id}`);
             
             // Remove the copy symbol
             const copySymbol = span.nextElementSibling; // Get the copy symbol
@@ -106,12 +48,16 @@ function toggleFields(editable) {
             span.parentNode.replaceChild(input, span);
         });
     } else {
+        // let updatedData = {};
         const inputs = document.querySelectorAll('.component-info-input');
         inputs.forEach(input => {
         if (input) {
             const newSpan = document.createElement('span');
             newSpan.className = 'component-info-input-span'; // Optional: add the original class for styling
             newSpan.innerText = input.value; // Save the value back to the new span
+            newSpan.id = input.id;
+            const key = newSpan.id.replace("component-info-input-", "");
+            currentComponent[key] = newSpan.innerText;
             
             // Replace the input with the new span
             input.parentNode.replaceChild(newSpan, input);
@@ -129,6 +75,12 @@ function toggleFields(editable) {
             }
         }
         });
+        // updatedData['id'] = currentComponentId;
+        // console.log(`New data: ${JSON.stringify(currentComponent, null, 2)}`);
+        await upate_component(currentComponent);
+        updateComponents(sortComponents(await fetchComponents()));
+        displayComponents(null);
+        showComponentInfo(currentComponent);
     }
 }
 
@@ -138,114 +90,173 @@ function toggleEdit() {
         button.innerText = 'Save';
         toggleFields(true);
     } else {
+        // upate_component();
         button.innerText = 'Edit';
         toggleFields(false);
     }
 }
 
-export function showComponentInfo(category, index, component) {
-    const componentData = {
-        "category": "Resistor",
-        "value": "10k",
-        "precision": "1%",
-        "package": "0603",
-        "technology": "Film",
-        "amount available": 10,
-        "description": "Standard 10k Ohm resistor.",
-        "KiCAD footprint": "path/to/footprint",
-        "KiCAD symbol": "path/to/symbol",
-        "image": "static/images/resistor_1w.jpg",
-        "3D model": "static/3d_models/octal_socket.glb"
-    }
-    const hasCopySymbolFields = ["KiCAD footprint", "KiCAD symbol", "3D model"];
+// Constants
+const COMP_FIELDS = {
+    "Resistors": ["value", "precision", "package", "technology", "description", "image", "kicad_footprint", "kicad_symbol", "model_3d", "amount"],
+    "Capacitors": ["value", "voltage", "package", "technology", "description", "image", "kicad_footprint", "kicad_symbol", "model_3d", "amount"],
+    "ICs": ["subcategory", "group_name", "name", "package", "technology", "description", "image", "kicad_footprint", "kicad_symbol", "model_3d", "amount"]
+};
+const COPY_SYMBOL_FIELDS = ["kicad_footprint", "kicad_symbol", "model_3d"];
 
-    let infoPanel = document.getElementById('right-section');
-    // infoPanel.id = 'component-details';
+// Main function
+export function showComponentInfo(componentData) {
+    initializeComponentInfo(componentData);
+    const infoPanel = document.getElementById('right-section');
+    
+    const componentInfoSection = createComponentInfoSection(componentData);
+    infoPanel.appendChild(componentInfoSection);
+    
+    const imageModelSection = createImageAndModelSection(componentData);
+    infoPanel.appendChild(imageModelSection);
+}
+
+// Initialize component data and display panel
+function initializeComponentInfo(componentData) {
+    currentComponent = componentData;
+    currentComponentId = componentData['id'];
+    
+    const infoPanel = document.getElementById('right-section');
     infoPanel.style.display = 'flex';
     infoPanel.innerHTML = '';
+}
 
-    // Component info:
+// Create the component info section
+function createComponentInfoSection(componentData) {
     const componentInfoDiv = document.createElement('div');
     componentInfoDiv.className = 'component-info';
+    
+    const header = createComponentInfoHeader(componentData);
+    componentInfoDiv.appendChild(header);
+    
+    const infoList = createComponentInfoList(componentData);
+    componentInfoDiv.appendChild(infoList);
+    
+    const editButton = createEditButton();
+    componentInfoDiv.appendChild(editButton);
+    
+    return componentInfoDiv;
+}
 
-        const componentHeader = document.createElement('h2');
-        componentHeader.innerText = "Component Details";
-        componentInfoDiv.appendChild(componentHeader);
+// Create the component info header
+function createComponentInfoHeader(componentData) {
+    const componentHeader = document.createElement('h2');
+    componentHeader.innerText = "Component Details";
+    
+    const componentName = document.createElement('h3');
+    componentName.innerText = componentData['description'];
+    
+    const headerContainer = document.createElement('div');
+    headerContainer.appendChild(componentHeader);
+    headerContainer.appendChild(componentName);
+    
+    return headerContainer;
+}
 
-        const componentName = document.createElement('h3');
-        componentName.innerText = "10k Ohm Resistor";   // TODO: UpdateMe!
-        componentInfoDiv.appendChild(componentName);
+// Create the component info list
+function createComponentInfoList(componentData) {
+    const componentInfoTable = document.createElement('ul');
+    
+    for (const parameter of COMP_FIELDS[componentData['category']]) {
+        const hasCopyButton = COPY_SYMBOL_FIELDS.includes(parameter);
+        const listItem = createComponentInfoListItem(
+            parameter, 
+            componentData[parameter], 
+            hasCopyButton
+        );
+        componentInfoTable.appendChild(listItem);
+    }
+    
+    return componentInfoTable;
+}
 
-        const componentInfoTable = document.createElement('ul');
+// Create a single component info list item
+function createComponentInfoListItem(parameter, value, hasCopyButton) {
+    const componentInfoTableItem = document.createElement('li');
+    
+    if (hasCopyButton) {
+        componentInfoTableItem.className = 'has-copy-symbol';
+    }
+    
+    const fieldName = document.createElement('strong');
+    fieldName.className = 'component-info-field-name';
+    fieldName.innerText = parameter;
+    componentInfoTableItem.appendChild(fieldName);
+    
+    const fieldValue = document.createElement('span');
+    fieldValue.className = 'component-info-input-span';
+    fieldValue.id = `component-info-input-${parameter}`;
+    fieldValue.innerText = value;
+    componentInfoTableItem.appendChild(fieldValue);
+    
+    if (hasCopyButton) {
+        const copySymbol = document.createElement('span');
+        copySymbol.className = 'copy-symbol';
+        copySymbol.title = 'Copy to clipboard';
+        copySymbol.innerHTML = '&#10064;';
+        copySymbol.onclick = function() { copyToClipboard(this); };
+        componentInfoTableItem.appendChild(copySymbol);
+    }
+    
+    return componentInfoTableItem;
+}
 
-        for (const parameter in componentData) {
-                const componentInfoTableItem = document.createElement('li');
-                if(hasCopySymbolFields.includes(parameter)) {
-                    componentInfoTableItem.className = 'has-copy-symbol';
-                }
-                const componentInfoTableItemStrong = document.createElement('strong');
-                componentInfoTableItemStrong.className = 'component-info-field-name';
-                componentInfoTableItemStrong.innerText = `${parameter}`;
-                componentInfoTableItem.appendChild(componentInfoTableItemStrong);
+// Create the edit button
+function createEditButton() {
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.id = 'component-info-edit-button';
+    editButton.onclick = () => {
+        toggleEdit();
+    };
+    return editButton;
+}
 
-                const componentInfoTableItemSpan = document.createElement('span');
-                componentInfoTableItemSpan.className = 'component-info-input-span';
-                componentInfoTableItemSpan.innerText = `${componentData[parameter]}`;
-                componentInfoTableItem.appendChild(componentInfoTableItemSpan);
-
-                if(hasCopySymbolFields.includes(parameter)) {
-                    const copySymbolSpan = document.createElement('span');
-                    copySymbolSpan.className = 'copy-symbol';
-                    copySymbolSpan.title = 'Copy to clipboard';
-                    copySymbolSpan.innerHTML = '&#10064;';
-                    copySymbolSpan.onclick = function() { copyToClipboard(this); };
-                    componentInfoTableItem.appendChild(copySymbolSpan);
-                }
-
-                componentInfoTable.appendChild(componentInfoTableItem);
-            }
-
-        componentInfoDiv.appendChild(componentInfoTable);
-        
-        // <button id="component-info-edit-button" onclick="toggleEdit()">Edit</button>
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.id = 'component-info-edit-button';
-        editButton.onclick = () => {
-            toggleEdit();
-        };
-        componentInfoDiv.appendChild(editButton);
-
-    infoPanel.appendChild(componentInfoDiv);
-
-    // Image and 3D model:
+// Create the image and 3D model section
+function createImageAndModelSection(componentData) {
     const imageModelContainer = document.createElement('div');
     imageModelContainer.className = 'image-and-model-container';
+    
+    const componentImage = createComponentImage(componentData);
+    imageModelContainer.appendChild(componentImage);
+    
+    const componentModel = createComponentModel(componentData);
+    imageModelContainer.appendChild(componentModel);
+    
+    return imageModelContainer;
+}
 
-        const componentImageDiv = document.createElement('div');
-        componentImageDiv.className = 'component-image-div';
-            const componentImage = document.createElement('img');
-            componentImage.src = componentData["image"];
-            componentImage.className = 'component-image';
-            componentImage.alt = '10k Ohm Resistor';    // TODO: UpdateMe!
-            // componentImage.id = 'newImageId';
-            componentImageDiv.appendChild(componentImage);
-        imageModelContainer.appendChild(componentImageDiv);
+// Create the component image
+function createComponentImage(componentData) {
+    const componentImageDiv = document.createElement('div');
+    componentImageDiv.className = 'component-image-div';
+    
+    const componentImage = document.createElement('img');
+    componentImage.src = componentData["image"];
+    componentImage.className = 'component-image';
+    componentImage.alt = componentData['description'];
+    
+    componentImageDiv.appendChild(componentImage);
+    return componentImageDiv;
+}
 
-        const componentModelDiv = document.createElement('div');
-        componentModelDiv.className = 'component-3d-model';
-            const modelViewer = document.createElement('model-viewer');
-            modelViewer.setAttribute('src', componentData["3D model"]);
-            modelViewer.setAttribute('alt', "3D model of 10k Ohm Resistor");
-            modelViewer.setAttribute('auto-rotate', '');
-            modelViewer.setAttribute('camera-controls', '');
-            modelViewer.setAttribute('exposure', '0.5');
-            componentModelDiv.appendChild(modelViewer);
-        imageModelContainer.appendChild(componentModelDiv);
-
-    infoPanel.appendChild(imageModelContainer);
-
-    // oldInfoPanel.parentNode.replaceChild(newSection, oldInfoPanel);
-    // // Force reflow
-    // newSection.offsetHeight; // Accessing this property forces a reflow
+// Create the 3D model viewer
+function createComponentModel(componentData) {
+    const componentModelDiv = document.createElement('div');
+    componentModelDiv.className = 'component-3d-model';
+    
+    const modelViewer = document.createElement('model-viewer');
+    modelViewer.setAttribute('src', componentData["model_3d"]);
+    modelViewer.setAttribute('alt', "3D model of the component");
+    modelViewer.setAttribute('auto-rotate', '');
+    modelViewer.setAttribute('camera-controls', '');
+    modelViewer.setAttribute('exposure', '0.5');
+    
+    componentModelDiv.appendChild(modelViewer);
+    return componentModelDiv;
 }
